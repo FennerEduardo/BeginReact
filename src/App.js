f03b763a8493f6fa4ed36d6d1c0460cb03e11f5c
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-25 13:18:40 -0500

    :fire: using state to process data and children components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-// first component
-function Hello(props) {
-  return <h2>{props.title}</h2>
-}
-// second component
-const Hello2 = props => <h2>{props.title}</h2>
-
-//third component
-class Title extends Component {
+class Contador extends Component {
+  // Agregando el state desde el constructor
+  constructor() {
+    super()
+    this.state = { contador: 1 }
+    // modificar el state usando el metodo setState
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 })
+    }, 1000)
+  }
+  // agregando el state fuera del constructor
+  // state = { contador: 1 }
   render() {
-    return <h2>{this.props.text}</h2>
+    // usando el state
+    return <ContadorNumero numero={this.state.contador} />
   }
 }
-// default props
-Title.defaultProps = {
-  text: 'Título por defecto'
+
+class ContadorNumero extends Component {
+  render() {
+    console.log('ContadorNumero render()')
+    return <span>{this.props.numero}</span>
+  }
 }
 
-class Text extends Component {
+class App extends Component {
   render() {
-    // props
-    const {
-      activated,
-      arrayNumber,
-      multiply,
-      number,
-      objectInfo,
-      text,
-      title
-    } = this.props
-    //variables
-    const texto2 = activated ? 'Cierto' : 'Falso'
-    const mappedNumbers = arrayNumber.map(n => n * 2)
-    const mappedNumbers2 = arrayNumber.map(multiply)
     return (
-      <div>
-        <p>{text}</p>
-        <p>{number}</p>
-        <p>{arrayNumber.join(', ')}</p>
-        <p>{objectInfo.key}</p>
-        <p>{mappedNumbers.join(', ')}</p>
-        <p>{mappedNumbers2.join(', ')}</p>
-        <p>{texto2}</p>
-        <p>{title}</p>
+      <div className="App">
+        <p>Primer componente con state</p>
+        <Contador />
       </div>
-    )
+    );
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Bienvenidos a React</h1>
-        {/* <Hello title="Hello from props" />
-        <Hello2 title="Hello from props 2" />
-        */}
-        <Title />{/*Renderiza el default prop */}
-        <Title text="Hola mundo" />{/*Renderiza el prop recibido */}
-        <Text
-          arrayNumber={[1, 2, 3, 4, 5, 6]}
-          activated
-          multiply={(number) => number * 3}
-          number={2}
-          text="Texto"
-          objectInfo={{ key: "first", key2: "second" }}
-          title={<h1>Hola mundo como prop</h1>}
-        />
-
-      </header>
-    </div>
-  );
 }
 
 export default App;
