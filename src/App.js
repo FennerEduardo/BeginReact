43fc24665151f75462511ec4f6c537b1f2910ab0
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-26 09:13:18 -0500

    :fire: Itering Arrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { Component } from 'react';
-import ConditionalSection from './sections/conditional';
-
 
 class App extends Component {
   render() {
+    const numbers = [1, 1, 3, 4, 5]
     return (
       <div className="App">
-      <ConditionalSection />
+        <h4>Trabajando con Listas</h4>
+        {numbers.map((number, index) => {
+          return <p key={index}>Soy el número {number}</p>
+        })}
       </div>
     );
   }
