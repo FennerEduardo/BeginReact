2b16f77d79f28b7192471140e5748dcb58a5581f
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-26 11:30:19 -0500

    :fire: Using children prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import React, { Component } from 'react';
-import Forms from './sections/forms';
 
+class Box extends Component {
+  render() {
+    return <div style={{ border: '1px solid #000', margin: 5, padding: 5 }}>
+      {this.props.children}
+    </div>
+  }
+}
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Forms />
+        <h4>Children props</h4>
+        <Box>Hola! soy un children  </Box>
+        <Box>Hola! soy un children 2  </Box>
       </div>
     );
   }
