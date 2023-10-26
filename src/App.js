b3b67d1d1ff5c3c27c59ab87214f679771b93d68
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-10-26 16:10:03 -0500

    :fire: first part of the mounted of component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,27 @@
 import './App.css';
 import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
 
-class Box extends Component {
-  render() {
-    return <div style={{ border: '1px solid #000', margin: 5, padding: 5 }}>
-      {this.props.children}
-    </div>
-  }
-}
-
-class Article extends Component {
-  //Using prop types
-  static propTypes = {
-    author: PropTypes.string.isRequired
-  }
-
-  render() {
-    const { author, children, date, title } = this.props
-    return (
-      <section>
-        <h2>{title}</h2>
-        {author && <p><em>Escrito por:</em> {author}</p>}
-        <Box>{date}</Box>
-        <article>{children}</article>
-      </section>
-    )
-  }
-}
 class App extends Component {
+  constructor(props) {
+    console.log('primero el constructor');
+    super(props)// llamar la constructor de component class
+    // inicializar el state del componente
+    this.state = { mensajeInicial: 'mensaje inicial' }
+    // bindear el contexto para el metodo handleClick
+    //this.handleClick = this.handleClick.bind(this)
+  }
+  // al cambiar el metodo por un arrow function no es necesario hacer el bind del constructor
+  handleClick = (e) => {
+    this.setState({ mensajeInicial: 'mensaje cambiado' })
+  }
 
   render() {
+    console.log('luego el render');
     return (
       <div className="App">
-        <h4>Children props</h4>
-        <Article
-         
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre app children'
-        >
-          <p>
-            El contenido que envolvemos dentro del componente Article será enviado a las this.props.children, <strong>Y mantiene las estiquetas o tags (rjx) HTMl  que se usen sin problemas de renderización o necesidad de escaparlas</strong>
-          </p>
-        </Article>
-        <Article
-          author='Fenner'
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre app children 2'
-        >
-          <p>
-            El contenido que envolvemos dentro del componente Article será enviado a las this.props.children, <strong>Y mantiene las estiquetas o tags (rjx) HTMl  que se usen sin problemas de renderización o necesidad de escaparlas</strong>
-          </p>
-        </Article>
-        <Article
-
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre app children 3'
-        >
-          <p>
-            El contenido que envolvemos dentro del componente Article será enviado a las this.props.children, <strong>Y mantiene las estiquetas o tags (rjx) HTMl  que se usen sin problemas de renderización o necesidad de escaparlas</strong>
-          </p>
-        </Article>
+        <h4>Ciclo Montaje: constructor</h4>
+        {this.state.mensajeInicial}
+        <button onClick={this.handleClick}>Cambiar mensaje</button>
       </div>
     );
   }
